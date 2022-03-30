f4c97e87d942b8784d2c350a45a76e81cf613f01
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-30 14:48:50 -0500

    repasted all the code to ensure no errors

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
-const { Client } = require('pg');
+// inside db/index.js
+const { Client } = require('pg'); // imports the pg module
+
+// supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox_dev');
 
 module.exports = {
   client,
-};
+}
