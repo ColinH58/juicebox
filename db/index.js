7b8b353ab8c0ccb6a0e2f4813cbf7b812806553c
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-31 14:39:21 -0500

    back up to here

--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ const { Client } = require('pg'); // imports the pg module
 // supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
+// Helper function from part 1
 async function getAllUsers() {
   const { rows } = await client.query(
     `SELECT id, username 
