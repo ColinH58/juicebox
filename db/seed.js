229647e68554e2f1a05f0f2de37bb0c8b17a57fa
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-31 14:38:02 -0500

    moved onto the next step

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,4 @@
 // inside db/seed.js
-
 // grab our client with destructuring from the export in index.js
 const { client } = require('./index');
 
@@ -8,11 +7,28 @@ async function testDB() {
     // connect the client to the database, finally
     client.connect();
 
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `)
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username varchar(255) UNIQUE NOT NULL,
+      password varchar(255) NOT NULL
+      );
+    `)
+    await client.query(`
+    INSERT INTO users (username, password)
+    VALUES
+    ('albert', 'bertie99'),
+    ('sandra', '2sandy4me'),
+    ('glamgal', 'soglam');
+    `)
     // queries are promises, so we can await them
-    const result = await client.query(`SELECT * FROM users;`);
+    const { rows } = await client.query(`SELECT * FROM users;`);
 
     // for now, logging is a fine way to see what's up
-    console.log(result);
+    console.log(rows);
   } catch (error) {
     console.error(error);
   } finally {
