451facd0be1e4ffdbe99c5ef7036603da2bc23cf
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-31 16:18:45 -0500

    making sure code matches assignment's code

--- db/seed.js
+++ db/seed.js
@@ -2,28 +2,8 @@
 const {
   client,
   getAllUsers,
-  createUser
+  createUser,
 } = require('./index');
-
-// new function, should attempt to create a few users
-async function createInitialUsers() {
-  try {
-    console.log("Starting to create users...");
-
-    const albert = await createUser({ username: 'albert', password: 'bertie99' });
-    const sandra = await createUser({ username: 'albert', password: '2sandy4me' });
-    const glamgal = await createUser({ username: 'albert', password: 'soglam' });
-
-    console.log(albert);
-    console.log(sandra);
-    console.log(glamgal);
-
-    console.log("Finished creating users!");
-  } catch(error) {
-    console.error("Error creating users!");
-    throw error;
-  }
-}
 
 async function dropTables() {
   try {
@@ -59,6 +39,21 @@ async function createTables() {
   }
 }
 
+async function createInitialUsers() {
+  try {
+    console.log("Starting to create users...");
+
+    await createUser({ username: 'albert', password: 'bertie99' });
+    await createUser({ username: 'sandra', password: '2sandy4me' });
+    await createUser({ username: 'glamgal', password: 'soglam' });
+
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
+    throw error;
+  }
+}
+
 async function rebuildDB() {
   try {
     client.connect();
@@ -85,7 +80,9 @@ async function testDB() {
   }
 }
 
+
 rebuildDB()
   .then(testDB)
   .catch(console.error)
   .finally(() => client.end());
+  
