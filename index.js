3c3e5df342b93313d239fa752e9cc524fbf121f6
Colin Hoolwerf <choolwerf@gmail.com>
2022-04-06 19:50:27 -0500

    working through part 2

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-// inside index.js
 const express = require("express");
 const server = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const apiRouter = require("./api");
 
+// require("dotenv").config();
+server.use(morgan("dev"));
+server.use(bodyParser.json());
+server.use(express.json());
+server.use("/api", apiRouter);
+
+server.get("/background/:color", (req, res, next) => {
+  res.send(`
+  <body style="background: ${req.params.color};">
+  <h1>Hello World</h1>
+  </body>
+  `);
+});
+
+//Tells the server to always call this function
 server.use((req, res, next) => {
   console.log("<____Body Logger START____>");
   console.log(req.body);
   console.log("<_____Body Logger END_____>");
-
+  
   next();
 });
 
-const apiRouter = require("./api");
-server.use("/api", apiRouter);
+const { client } = require("./db");
+client.connect();
 
 const PORT = 3000;
 server.listen(PORT, () => {
